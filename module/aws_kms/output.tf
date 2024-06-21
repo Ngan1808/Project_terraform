68839635b833fe246d15bf40027b9bd6994f32f0
NguyenThiKimNgan <107528091+Ngan1808@users.noreply.github.com>
2024-06-21 15:38:07 +0700

    Update output.tf

--- module/aws_kms/output.tf
+++ module/aws_kms/output.tf
@@ -1,65 +1,15 @@
-# Outputs for AWS KMS Key
-output "kms_key_id" {
-  description = "The ID of the AWS KMS key"
-  value       = aws_kms_key.this[0].key_id
-}
-
 output "kms_key_arn" {
-  description = "The ARN of the AWS KMS key"
-  value       = aws_kms_key.this[0].arn
+  value = aws_kms_key.this.arn
 }
 
-output "kms_key_alias" {
-  description = "The alias of the AWS KMS key"
-  value       = aws_kms_alias.this[0].id
+output "kms_key_id" {
+  value = aws_kms_key.this.key_id
 }
 
-output "kms_key_policy" {
-  description = "The policy JSON of the AWS KMS key"
-  value       = aws_kms_key.this[0].policy
+output "kms_alias_arn" {
+  value = aws_kms_alias.this.arn
 }
 
-# Outputs for AWS KMS External Key
-output "kms_external_key_id" {
-  description = "The ID of the AWS KMS external key"
-  value       = aws_kms_external_key.this[0].id
-}
-
-output "kms_external_key_arn" {
-  description = "The ARN of the AWS KMS external key"
-  value       = aws_kms_external_key.this[0].arn
-}
-
-# Outputs for AWS KMS Replica Key
-output "kms_replica_key_id" {
-  description = "The ID of the AWS KMS replica key"
-  value       = aws_kms_replica_key.this[0].key_id
-}
-
-output "kms_replica_key_arn" {
-  description = "The ARN of the AWS KMS replica key"
-  value       = aws_kms_replica_key.this[0].arn
-}
-
-# Outputs for AWS KMS Replica External Key
-output "kms_replica_external_key_id" {
-  description = "The ID of the AWS KMS replica external key"
-  value       = aws_kms_replica_external_key.this[0].id
-}
-
-output "kms_replica_external_key_arn" {
-  description = "The ARN of the AWS KMS replica external key"
-  value       = aws_kms_replica_external_key.this[0].arn
-}
-
-# Outputs for AWS KMS Grants
-output "kms_grants" {
-  description = "Map of AWS KMS grants"
-  value       = aws_kms_grant.this
-}
-
-# Outputs for AWS KMS Aliases
-output "kms_aliases" {
-  description = "Map of AWS KMS aliases"
-  value       = aws_kms_alias.this
+output "kms_alias_name" {
+  value = aws_kms_alias.this.name
 }
